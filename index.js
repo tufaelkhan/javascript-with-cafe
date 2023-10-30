04adc3f0ca252af1f1c8e44611aced8c5837dfa2
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-29 18:39:24 -0700

    add problem

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-let obj ={
-    a: "tufael khan",
-    b: 23,
-    c: true
-}
-obj.a;
-obj.b;
-obj.c;
-obj["a"];
-obj["b"]
-obj["c"]
+// let obj ={
+//     a: "tufael khan",
+//     b: 23,
+//     c: true
+// }
+// obj.a;
+// obj.b;
+// obj.c;
+// obj["a"];
+// obj["b"]
+// obj["c"]
+
+// let obj = {
+//     a: "tufael khan",
+//     b: 23
+// }
+// let b = "a";
+// obj[b];
+// obj["b"];
+
+// let arr = ['tufael khan', 23, true]
+// arr[0];
+// arr[1];
+// arr[2];
+// arr.length;
+// typeof arr;
+
+// let a;
+// typeof a;
+// a = 23;
+// typeof a;
+// a = "tufael khan"
+// typeof a;
+// a = true;
+// typeof a;
+// a = null;
+// typeof a;
+// a = undefined;
+// typeof a;
+// a = { b: "c"}
+// typeof a;
+
+// let a = "23";
+// let b = 23;
+// a == b;
+// a === b;
+
